66fbab6c30c37e91f021462fd87b43d3c0e89784
mmoo758 <yccao94@gmail.com>
2026-07-22 13:25:39 +0800

    Revert "xl pandatest from mr.one"
    This reverts commit ab011f256ba9be6d9327cc97074c68bf20a75737.

--- python/__init__.py
+++ python/__init__.py
@@ -607,8 +607,7 @@ class Panda:
     return bytes(part_1 + part_2)
 
   def get_type(self):
-    #return self._handle.controlRead(Panda.REQUEST_IN, 0xc1, 0, 0, 0x40)
-    return bytearray(b'\x09')
+    return self._handle.controlRead(Panda.REQUEST_IN, 0xc1, 0, 0, 0x40)
 
   def get_packets_versions(self):
     dat = self._handle.controlRead(Panda.REQUEST_IN, 0xdd, 0, 0, 8)
